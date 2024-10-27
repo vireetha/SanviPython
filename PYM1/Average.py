82ec9e684c38a21858cd6b99d99006f64f920393
vireetha <dudalaviree69@gmail.com>
2024-10-28 05:11:24 +0530

    average

---
+++ PYM1/Average.py
@@ -0,0 +1,11 @@
+tree1 = 98
+tree2 = 94
+tree3 = 86
+tree4 = 41
+tree5 = 21
+
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all the 5 trees is: ",sum)
+
+average = sum/5
+print("The average of all the tree is: ",average)
